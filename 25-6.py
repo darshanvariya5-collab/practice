b2c0bb169bc50bc4ae7c7e3c59992ebd060b74c0
darshanvariya5-collab <darshanvariya5@gmail.com>
2026-06-25 16:41:18 +0530

    Add files via upload

---
+++ 25-6.py
@@ -0,0 +1,93 @@
+#ooperater variable amd data type
+#1. aritmetic operater
+
+'''
+a=20
+b=30
+print('addition:',a+b)
+print("sub:",a-b)
+print("multi:",a*b)
+print("divi:",a/b)
+print("modulus:",a%b)
+print("exponentiation:",a**b)
+print("floor division:",a//b)
+'''
+
+#2 assignment oprater
+'''
+x=5
+y=3
+'''
+'''
+x+=y
+print (x)
+print (y)
+x-=y
+print(x)
+print (y)
+
+x/=y
+print(x)
+print(y)
+
+x%=y
+print(x)
+print(y)
+'''
+'''
+x**=y
+print(x)
+print(y)
+'''
+'''
+x//=y
+print (x)
+print(y)
+'''
+
+
+#comarision operater
+'''
+x=10
+x=10
+print(x==10)
+print(x!=y)
+print(x<y)
+print(x<=y)
+print(x>y)
+print(x>=y)
+print(x=y)
+'''
+
+
+#logical oprater(and or  not)
+'''
+x=True
+y=False
+z=False
+a=True
+print(x and y)
+print(y and z)
+print(x or y)
+print(y or z)
+print(x or a)
+print(not x)
+print (not y)
+'''
+
+
+
+
+'''#type conversion
+num_str="123456"
+num(type(num_str))
+num_int=int(num_str)
+print(type (mum_int))
+print(num_int +7)
+'''
+
+a="1,2,3"
+print (type (a))
+
+b= (12456)
+print (type(b))
