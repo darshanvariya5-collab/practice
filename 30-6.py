d5dd8446bfb13a11bbbc54b5865a15d61a4eff77
darshanvariya5-collab <darshanvariya5@gmail.com>
2026-06-30 17:00:22 +0530

    Add files via upload

---
+++ 30-6.py
@@ -0,0 +1,81 @@
+#loop in the python
+
+
+'''
+1.while loop
+'''
+'''
+i=1
+while i<=10:
+    print(i)
+    i+=2
+    
+'''
+'''
+i=5
+while i>=1:
+    print(i)
+    i+=2
+   '''
+#for loop
+
+# for variable in sequence
+# code block
+
+#print number 1 to5
+'''
+for i in range (1,6):
+      print(i)
+'''
+
+#print for loop in string
+'''
+name="darshan"
+for i in name:
+          print(i)
+'''
+# print for loop in list
+'''
+fruits=("apple","orange","banana","mango")
+for item in fruits:
+  print(item)
+'''
+
+#range function
+     #(start, stop, step)
+'''
+for i in range (5):
+ print(i)
+
+for i in range (1,6):
+   print(i)
+'''
+'''
+for i in range(0,10,3):
+ print(i)
+'''
+'''
+for i in range (10,0,-1):
+   print(i)
+'''
+
+#nested loop in python
+   #loop in side another loop
+   #tables
+   #pattens
+   #matrices
+
+for i in range(1,6+1):
+     for j in range(1,4):
+         print(j,end=" ")
+     print()
+
+'''
+#break statment
+for i in range(1,7):
+  if i==4:
+      break
+print(i)
+   ''' 
+    
+    
